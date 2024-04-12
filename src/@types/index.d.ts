d328b109837943047d1b4a57a669b51af90f83d9
itsokey76 <itsokey086@gmail.com>
2024-04-12 12:12:29 +0300

    Initial commit

---
+++ src/@types/index.d.ts
@@ -0,0 +1,21 @@
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.module.scss'
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.scss'
+
+function App() {
+
+  return (
+    <>
+     <div className={styles.root}>Template</div>
+    </>
+  )
+}
+
+export default App
